31746fb5e32ecf6693bac32d1c9d5f0899b9e8b4
Mohangit2810 <mohanraji2810@gmail.com>
2024-05-07 13:43:51 +0530

    test changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,22 +4,6 @@ import App from "./App.jsx";
 import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const isInstagramBrowser = window.navigator.userAgent.includes("Instagram");
-
-console.log(window.navigator.userAgent);
-
-if (isInstagramBrowser) {
-  const links = document.querySelectorAll("a");
-
-  links.forEach((link) => {
-    link.addEventListener("click", (event) => {
-      event.preventDefault();
-      const url = link.getAttribute("href");
-      window.open(url, "_system");
-    });
-  });
-}
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <GoogleOAuthProvider clientId="173648640509-tj51tppmc4i2trdn7qjhujvescnpmim1.apps.googleusercontent.com">
     <React.StrictMode>
